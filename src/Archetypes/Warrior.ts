24fb255571bd018a82269c4e028f71a8455fe1f7
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-02-17 13:59:57 -0300

    feat: criando a classe Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static numberCreatedArchetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior.numberCreatedArchetypeInstances += 1;
+  }
+
+  override get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances() {
+    return Warrior.numberCreatedArchetypeInstances;
+  }
+}
