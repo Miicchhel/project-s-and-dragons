c0bd9134603ccdb6477c4ddd54bcf264c4014ba1
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-02-19 21:21:13 -0300

    feat: criando as funções de battle e attackPlayer para o PVP

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,20 @@
+import Fighter from './Fighter';
+
 function getRandomInt(min: number, max: number) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
+}
+
+function attackPlayer(player1:Fighter, player2:Fighter) {
+  const attack = getRandomInt(1, 10);
+  return attack % 2 === 0 ? player1.attack(player2) : player2.attack(player1);
+}
+
+export function battle(player1:Fighter, player2:Fighter) {
+  while (player1.lifePoints > 0 && player2.lifePoints > 0) {
+    attackPlayer(player1, player2);
+  }
 }
 
 export default getRandomInt;
