c0faceba5d01c6a0c1c38e7e0a9174d28d1c1909
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-02-21 21:12:49 -0300

    refactor: refatorando as funções battle e attackPlayer, de utils, para poder batalhar também com os monstros

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 
 function getRandomInt(min: number, max: number) {
   const newMin = Math.ceil(min);
@@ -6,12 +6,12 @@ function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
 
-function attackPlayer(player1:Fighter, player2:Fighter) {
+function attackPlayer(player1:Fighter, player2:Fighter | SimpleFighter) {
   const attack = getRandomInt(1, 10);
   return attack % 2 === 0 ? player1.attack(player2) : player2.attack(player1);
 }
 
-export function battle(player1:Fighter, player2:Fighter) {
+export function battle(player1:Fighter, player2:Fighter | SimpleFighter) {
   while (player1.lifePoints > 0 && player2.lifePoints > 0) {
     attackPlayer(player1, player2);
   }
